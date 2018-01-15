107c525381f6318c653abac4dde885b564fce5a4
Xian Zhuo <1722741150@qq.com>
2018-01-15 15:09:35 +0800

    deploy on docker

--- ui/build.sh
+++ ui/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 echo "### Build docker image ..."
-docker build -t em-game-ui:latest .
+docker build -t mem-game-ui:latest .
 
 echo "### Package docker image ..."
-docker save em-game-ui:latest | gzip > em-game-ui.tgz
+docker save mem-game-ui:latest | gzip > mem-game-ui.tgz
 echo "### Package docker image finish."
+
